59c5c5bc049a12285eecbf11d0f6bece02073083
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-01-14 21:43:50 +0100

    Add stop_when method to Iterator implementations in solution for Day4

--- rust/advent of code/day4/src/main.rs
+++ rust/advent of code/day4/src/main.rs
@@ -4,15 +4,29 @@ use std::io;
 use std::io::Read;
 use md5::Digest;
 
-struct StopWhen<I, P> {
+struct StopWhenIterator<I, P> {
     parent: I,
     predicate: P,
     completed: bool
 }
 
-impl<I, P> Iterator for StopWhen<I, P>
+trait StopWhen<P: FnMut(&Self::Item) -> bool> : Iterator
+    where Self:Sized {
+
+    /// Iterates over items untill while the given predicate is evaluated to true
+    /// Once predicate is evaluated to false, iterator yields current item and completes.
+    fn stop_when(self, predicate: P) -> StopWhenIterator<Self, P> {
+        StopWhenIterator {parent: self, predicate: predicate, completed: false}
+    }
+}
+
+impl<I, P> StopWhen<P> for I
     where P: FnMut(&I::Item) -> bool,
-    I: Iterator {
+          I : Iterator { }
+
+impl<I, P> Iterator for StopWhenIterator<I, P>
+    where P: FnMut(&I::Item) -> bool,
+          I: Iterator {
 
     type Item = I::Item;
 
@@ -28,14 +42,6 @@ impl<I, P> Iterator for StopWhen<I, P>
             })
         }
     }
-}
-
-/// Iterates over items untill while the given predicate is evaluated to true
-/// Once predicate is evaluated to false, iterator yields current item and completes.
-fn stop_when<I, P>(iterator: I, predicate: P) -> StopWhen<I, P>
-    where P: FnMut(&I::Item) -> bool,
-    I: Iterator {
-        StopWhen {parent: iterator, predicate: predicate, completed: false}
 }
 
 fn read_input() -> io::Result<String> {
@@ -55,15 +61,14 @@ fn sixth_item_is_also_zero(digest: &Digest) -> bool {
 fn main() {
     let input = read_input().unwrap();
 
-    let start_with_zero = (0..i64::max_value())
+    let start_with_zero: Vec<_> = (0..i64::max_value())
         .map(|i| (i, input.clone() + &i.to_string()))
         .map(|(i, s)| (i, md5::compute(s.as_bytes())))
-        .filter(|&(_, digest)| starts_with_five_zeros(&digest));
-
-    let all: Vec<_> = stop_when(start_with_zero, |&(_, digest)| sixth_item_is_also_zero(&digest))
+        .filter(|&(_, digest)| starts_with_five_zeros(&digest))
+        .stop_when(|&(_, digest)| sixth_item_is_also_zero(&digest))
         .map(|(i, _)| i)
         .collect();
 
-    println!("Smallest with 5 zeroes: {}", all[0]);
-    println!("Smallest with 6 zeroes: {}", all[all.len() - 1]);
+    println!("Smallest with 5 zeroes: {}", start_with_zero[0]);
+    println!("Smallest with 6 zeroes: {}", start_with_zero[start_with_zero.len() - 1]);
 }
