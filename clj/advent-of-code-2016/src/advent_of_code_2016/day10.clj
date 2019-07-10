76d59826f148cb31910d63e1e5b9e89b14214201
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-07-10 18:27:10 +0300

    Still WIP: AOC 10 part 1

--- clj/advent-of-code-2016/src/advent_of_code_2016/day10.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/day10.clj
@@ -2,6 +2,9 @@
 
 (def input "value 5 goes to bot 2\nbot 2 gives low to bot 1 and high to bot 0\nvalue 3 goes to bot 1\nbot 1 gives low to output 1 and high to bot 0\nbot 0 gives low to output 2 and high to output 0\nvalue 2 goes to bot 2")
 
+
+(def initial-state {:output {} :bot {}})
+
 (defn parse-line
   [line]
   (if-let [[_ val bot] (re-matches #"value (\d+) goes to bot (\d+)" line)]
@@ -10,15 +13,20 @@
       {:bot (Integer/parseInt bot) :low-type (keyword low-type) :low (Integer/parseInt low) :high-type (keyword high-type) :high (Integer/parseInt high)}
       (throw (Exception. (str "Invalid input: " line))))))
 
+(defn process-direct
+  [state {value :value bot :bot}]
+  (let [bots-state (merge-with clojure.set/union (:bot state) {bot #{value}})]
+    (assoc state :bot bots-state)))
+
 (defn process-instruction
   [state instruction]
-  (if-let [val (:value instruction)]
-    (merge-with clojure.set/union state {(:bot instruction) #{val}})
+  (if (:value instruction)
+    (process-direct state instruction)
     state))
 
 (defn process-instructions
   [instructions]
-  (reduce process-instruction {} instructions))
+  (reduce process-instruction initial-state instructions))
 
 (defn parse-input
   [input]
