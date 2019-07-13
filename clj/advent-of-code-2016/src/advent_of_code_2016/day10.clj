b6004f24e5e10a1c8abaae501a348cd6e5269166
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-07-13 16:49:45 +0300

    AOC day 10

--- clj/advent-of-code-2016/src/advent_of_code_2016/day10.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/day10.clj
@@ -1,8 +1,5 @@
 (ns advent-of-code-2016.day10)
 
-(def input "value 5 goes to bot 2\nbot 2 gives low to bot 1 and high to bot 0\nvalue 3 goes to bot 1\nbot 1 gives low to output 1 and high to bot 0\nbot 0 gives low to output 2 and high to output 0\nvalue 2 goes to bot 2")
-
-
 (def initial-state {:output {} :bot {}})
 
 (defn parse-line
@@ -10,26 +7,74 @@
   (if-let [[_ val bot] (re-matches #"value (\d+) goes to bot (\d+)" line)]
     {:value (Integer/parseInt val) :bot (Integer/parseInt bot)}
     (if-let [[_ bot low-type low high-type high] (re-matches #"bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)" line)]
-      {:bot (Integer/parseInt bot) :low-type (keyword low-type) :low (Integer/parseInt low) :high-type (keyword high-type) :high (Integer/parseInt high)}
+      {:bot (Integer/parseInt bot) :low-type (keyword low-type) :low-dest (Integer/parseInt low) :high-type (keyword high-type) :high-dest (Integer/parseInt high)}
       (throw (Exception. (str "Invalid input: " line))))))
+
+(defn try-include
+  [state value & path]
+  (let [current (get-in state path #{})]
+    (if (> 2 (count current))
+      (assoc-in state path (conj current value) )
+      state)))
 
 (defn process-direct
   [state {value :value bot :bot}]
-  (let [bots-state (merge-with clojure.set/union (:bot state) {bot #{value}})]
-    (assoc state :bot bots-state)))
+  (try-include state value :bot bot))
+
+(defn process-comparative
+  [state {bot :bot low-type :low-type low-dest :low-dest high-type :high-type high-dest :high-dest}]
+  (let [values (get-in state [:bot bot])]
+    (if (= 2 (count values))
+      (let [low-value (apply min values)
+            high-value (apply max values)]
+        (-> state
+            (try-include low-value low-type low-dest)
+            (try-include high-value high-type high-dest)
+            (assoc-in [:bot bot] #{})))
+      state)))
 
 (defn process-instruction
   [state instruction]
-  (if (:value instruction)
-    (process-direct state instruction)
-    state))
+  (cond
+    (:value instruction) (process-direct state instruction)
+    (:low-type instruction) (process-comparative state instruction)
+    :else state))
 
-(defn process-instructions
-  [instructions]
-  (reduce process-instruction initial-state instructions))
+(defn find-bot
+  [required-values state]
+  (some (fn [[bot bot-values]] (and (= bot-values required-values) bot)) (:bot state)))
+
+(defn require-outputs
+  [required-outputs state]
+  (let [available-outputs (:output state)
+        required-output-values (map (fn [output-key] (get available-outputs output-key)) required-outputs)]
+    (if (some nil? required-output-values) nil required-output-values)))
+
+(defn repeat-instructions
+  [state instructions stop-when]
+  (let [cycled-instructions (cycle instructions)]
+    (loop [current-state state [current-instruction & rest-instructions] cycled-instructions]
+      (if-let [stop-result (stop-when current-state)]
+        stop-result
+        (let [new-state (process-instruction current-state current-instruction)]
+          (recur new-state rest-instructions))))))
 
 (defn parse-input
   [input]
   (->> input
        (clojure.string/split-lines)
        (map parse-line)))
+
+(defn solve
+  [raw-input stop-when]
+  (let [instructions (parse-input raw-input)]
+    (repeat-instructions initial-state instructions stop-when)))
+
+(defn solve-part1
+  [raw-input]
+  (solve raw-input (partial find-bot #{17 61})))
+
+(defn solve-part2
+  [raw-input]
+  (let [output-values (solve raw-input (partial require-outputs [0 1 2]))]
+    (apply * (flatten (map seq output-values)))))
