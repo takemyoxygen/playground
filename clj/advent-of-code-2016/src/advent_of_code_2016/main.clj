e048219be62fcdbc68682e4266ed50c1e922e3db
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-08-11 23:25:48 +0300

    AOC day 14

--- clj/advent-of-code-2016/src/advent_of_code_2016/main.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/main.clj
@@ -4,7 +4,8 @@
             [advent-of-code-2016.day10 :as day10]
             [advent-of-code-2016.day11 :as day11]
             [advent-of-code-2016.day12 :as day12]
-            [advent-of-code-2016.day13 :as day13]))
+            [advent-of-code-2016.day13 :as day13]
+            [advent-of-code-2016.day14 :as day14]))
 
 (defn -main
   "I don't do a whole lot ... yet."
