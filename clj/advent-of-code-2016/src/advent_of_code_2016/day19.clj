631ba81dcedd07344393172fcd8af0d9dfe3b8a6
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-08-21 18:02:52 +0300

    Still WIP: AOC 19

--- clj/advent-of-code-2016/src/advent_of_code_2016/day19.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/day19.clj
@@ -21,9 +21,9 @@
 
 (defn step
   [all i]
-  (println all i)
+  ;(println all i)
   (let [steal-from (rem (+ i (quot (count all) 2)) (count all))]
-    (println (str "Elf " (all i) " steals from elf " (all steal-from)))
+    ;(println (str "Elf " (all i) " steals from elf " (all steal-from)))
     (filterv #(not= % (all steal-from)) all)))
 
 (defn simulate
@@ -34,3 +34,24 @@
         (let [next-elves (step elves index)
               next-index (if (> (count next-elves) (inc index)) (inc index) 0)]
           (recur next-elves next-index)))))
+
+(defn step-2
+  [all]
+  (let [total (count all)
+        indices (range total)
+        steal-from-indices (map (fn [i] (+ i i (quot (- total i) 2))) indices)
+        valid-steal-from-indices (take-while #(> total %) steal-from-indices)
+        elves-to-skip (apply hash-set (map all valid-steal-from-indices))
+        partition-index (count valid-steal-from-indices)
+        [processed not-processed] (split-at partition-index all)
+        filtered-not-processed (filter #(not (contains? elves-to-skip %)) not-processed)
+        next-all (concat filtered-not-processed processed)]
+    ;(println (map all valid-steal-from-indices))
+    (vec next-all)))
+
+(defn simulate-2
+  [n]
+  (->> (all-elves n)
+       (iterate step-2)
+       (drop-while #(> (count %) 1))
+       (first)))
