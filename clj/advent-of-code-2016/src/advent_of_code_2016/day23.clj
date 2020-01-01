40aa832493d62e059ce461e3dd3d61e2994d8d59
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2020-01-01 21:43:19 +0300

    AOC - move common monorail-related code from day 23 to shared module

--- clj/advent-of-code-2016/src/advent_of_code_2016/day23.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/day23.clj
@@ -1,110 +1,6 @@
 (ns advent-of-code-2016.day23
   (:require [advent-of-code-2016.monorail :as monorail]))
 
-(defn get-initial-state
-  [instructions]
-  {:next 0 :instructions (vec instructions) :registers {}})
-
-(defn resolve-value
-  [operand state]
-  (if (= (:type operand) :constant)
-    (:value operand)
-    (get-in state [:registers (:name operand)] 0)))
-
-(defn move-next
-  [state]
-  (update state :next inc))
-
-(defn toggle-instruction
-  [{:keys [name operands] :as instruction}]
-  (cond
-    (and (= 1 (count operands)) (= "inc" name)) {:name "dec" :operands operands}
-    (= 1 (count operands)) {:name "inc" :operands operands}
-    (and (= 2 (count operands)) (= "jnz" name)) {:name "cpy" :operands operands}
-    (= 2 (count operands)) {:name "jnz" :operands operands}
-    :else instruction))
-
-(defn set-register
-  [register value state]
-  (assoc-in state [:registers (:name register)] value))
-
-(defn update-register
-  [register f state]
-  (let [current (resolve-value register state)]
-    (set-register register (f current) state)))
-
-(defn perform-tgl
-  [{[operand] :operands} {:keys [instructions] :as state}]
-  (let [instruction-index (+ (:next state) (resolve-value operand state))
-        new-instructions (if
-                           (contains? instructions instruction-index)
-                           (update instructions instruction-index toggle-instruction)
-                           instructions)]
-    (-> state
-        (assoc :instructions new-instructions)
-        (move-next))))
-
-(defn perform-cpy
-  [{[op1 op2] :operands} state]
-  (if
-    (monorail/register? op2)
-    (->> state
-         (set-register op2 (resolve-value op1 state))
-         move-next)
-    (move-next state)))
-
-(defn perform-inc
-  [{[op] :operands} state]
-  (if
-    (monorail/register? op)
-    (->> state
-         (update-register op inc)
-         move-next)
-    (move-next state)))
-
-(defn perform-dec
-  [{[op] :operands} state]
-  (if
-    (monorail/register? op)
-    (->> state
-         (update-register op dec)
-         move-next)
-    (move-next state)))
-
-(defn perform-mult
-  [{[op1 op2] :operands} state]
-  (let [result (* (resolve-value op1 state) (resolve-value op2 state))]
-    (->> state
-         (set-register op1 result)
-         (move-next))))
-
-(defn perform-jnz
-  [{[op steps] :operands} state]
-  (let [steps-value (resolve-value steps state)
-        registry-value (resolve-value op state)]
-    (if (= registry-value 0)
-      (move-next state)
-      (update state :next (partial + steps-value)))))
-
-(defn perform-operation
-  [operation state]
-  (case (:name operation)
-    "cpy" (perform-cpy operation state)
-    "inc" (perform-inc operation state)
-    "dec" (perform-dec operation state)
-    "jnz" (perform-jnz operation state)
-    "tgl" (perform-tgl operation state)
-    "mult" (perform-mult operation state)
-    (throw (Exception. (str "Unknown operation" operation)))))
-
-(defn execute-instructions
-  [state]
-  (loop [{:keys [instructions next] :as current-state} state]
-    (if (contains? instructions next)
-      (recur (perform-operation (instructions next) current-state))
-      current-state)))
-
-
 ; several instructions in the original input are replaced with new "mult" instruction followed
 ; by setting a few registers to 0 and then jumping to the end of the multiplication (to keep the total number of instructions)
 ; the same as in the original input
@@ -113,17 +9,17 @@
 ; Part 1
 (->> input
      (monorail/parse-instructions)
-     (get-initial-state)
-     (set-register {:name "a"} 7)
-     (execute-instructions)
+     (monorail/get-empty-state)
+     (monorail/set-register {:name "a"} 7)
+     (monorail/execute-instructions)
      (:registers)
      (println "Part 1"))
 
 ; Part 2
 (->> input
      (monorail/parse-instructions)
-     (get-initial-state)
-     (set-register {:name "a"} 12)
-     (execute-instructions)
+     (monorail/get-empty-state)
+     (monorail/set-register {:name "a"} 12)
+     (monorail/execute-instructions)
      (:registers)
      (println "Part 2"))
--- clj/advent-of-code-2016/src/advent_of_code_2016/monorail.clj
+++ clj/advent-of-code-2016/src/advent_of_code_2016/monorail.clj
@@ -26,3 +26,107 @@
 (defn constant?
   [{:keys [type]}]
   (= type :constant))
+
+(defn get-empty-state
+  [instructions]
+  {:next 0 :instructions (vec instructions) :registers {}})
+
+(defn resolve-value
+  [operand state]
+  (if (= (:type operand) :constant)
+    (:value operand)
+    (get-in state [:registers (:name operand)] 0)))
+
+(defn move-next
+  [state]
+  (update state :next inc))
+
+(defn set-register
+  [register value state]
+  (assoc-in state [:registers (:name register)] value))
+
+(defn update-register
+  [register f state]
+  (let [current (resolve-value register state)]
+    (set-register register (f current) state)))
+
+
+(defn toggle-instruction
+  [{:keys [name operands] :as instruction}]
+  (cond
+    (and (= 1 (count operands)) (= "inc" name)) {:name "dec" :operands operands}
+    (= 1 (count operands)) {:name "inc" :operands operands}
+    (and (= 2 (count operands)) (= "jnz" name)) {:name "cpy" :operands operands}
+    (= 2 (count operands)) {:name "jnz" :operands operands}
+    :else instruction))
+
+(defn perform-tgl
+  [{[operand] :operands} {:keys [instructions] :as state}]
+  (let [instruction-index (+ (:next state) (resolve-value operand state))
+        new-instructions (if
+                           (contains? instructions instruction-index)
+                           (update instructions instruction-index toggle-instruction)
+                           instructions)]
+    (-> state
+        (assoc :instructions new-instructions)
+        (move-next))))
+
+(defn perform-cpy
+  [{[op1 op2] :operands} state]
+  (if
+    (register? op2)
+    (->> state
+         (set-register op2 (resolve-value op1 state))
+         move-next)
+    (move-next state)))
+
+(defn perform-inc
+  [{[op] :operands} state]
+  (if
+    (register? op)
+    (->> state
+         (update-register op inc)
+         move-next)
+    (move-next state)))
+
+(defn perform-dec
+  [{[op] :operands} state]
+  (if
+    (register? op)
+    (->> state
+         (update-register op dec)
+         move-next)
+    (move-next state)))
+
+(defn perform-mult
+  [{[op1 op2] :operands} state]
+  (let [result (* (resolve-value op1 state) (resolve-value op2 state))]
+    (->> state
+         (set-register op1 result)
+         (move-next))))
+
+(defn perform-jnz
+  [{[op steps] :operands} state]
+  (let [steps-value (resolve-value steps state)
+        registry-value (resolve-value op state)]
+    (if (= registry-value 0)
+      (move-next state)
+      (update state :next (partial + steps-value)))))
+
+(defn perform-operation
+  [operation state]
+  (case (:name operation)
+    "cpy" (perform-cpy operation state)
+    "inc" (perform-inc operation state)
+    "dec" (perform-dec operation state)
+    "jnz" (perform-jnz operation state)
+    "tgl" (perform-tgl operation state)
+    "mult" (perform-mult operation state)
+    (throw (Exception. (str "Unknown operation" operation)))))
+
+(defn execute-instructions
+  [state]
+  (loop [{:keys [instructions next] :as current-state} state]
+    (if (contains? instructions next)
+      (recur (perform-operation (instructions next) current-state))
+      current-state)))
