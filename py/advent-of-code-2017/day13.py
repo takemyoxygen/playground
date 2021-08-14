7e7c2cddf0e19d1be82b1fbe8a357988824f227f
Vladimir Makarov <makarov@targetprocess.com>
2021-08-14 09:28:42 +0300

    Improve AOC 2017 - day13

--- py/advent-of-code-2017/day13.py
+++ py/advent-of-code-2017/day13.py
@@ -1,5 +1,6 @@
 from typing import List
 import itertools
+import time
 
 
 def parse_input(lines: List[str]) -> dict[int, int]:
@@ -10,35 +11,51 @@ def parse_input(lines: List[str]) -> dict[int, int]:
     return layers
 
 
-def get_scanner_position(time: int, depth: int) -> int:
+def scanner_is_at_the_top(time: int, depth: int) -> bool:
     period = 2 * depth - 2
-    period_pos = time % period
-    return period_pos if period_pos < depth else period - period_pos
+    return time % period == 0
 
 
-def calculate_severity(layers: dict[int, int], delay: int) -> tuple[int, int]:
+def calculate_severity(layers: dict[int, int], delay: int) -> int:
     severity = 0
-    times_caught = 0
     for layer in layers:
         depth = layers[layer]
-        scanner_position = get_scanner_position(layer + delay, depth)
-        if scanner_position == 0:
+        if scanner_is_at_the_top(layer + delay, depth):
             severity += layer * depth
-            times_caught += 1
-    return severity, times_caught
+    return severity
+
+
+def to_remainder_constraints(layers: dict[int, int]) -> List[tuple[int, int]]:
+    constraints = []
+    for layer in layers:
+        period = 2 * layers[layer] - 2
+        remainder = (period - (layer % period)) % period
+        constraints.append((period, remainder))
+    return constraints
+
+
+def satisfies_constraints(n: int, constraints: List[tuple[int, int]]) -> bool:
+    for val, rem in constraints:
+        if n % val == rem:
+            return False
+    return True
 
 
 def part1(layers: dict[int, int]) -> int:
-    return calculate_severity(layers, 0)[0]
+    return calculate_severity(layers, 0)
 
 
 def part2(layers: dict[int, int]) -> int:
     delays = itertools.count()
-    return next(delay for delay in delays if calculate_severity(layers, delay)[1] == 0)
+    constraints = to_remainder_constraints(layers)
+    return next(delay for delay in delays if satisfies_constraints(delay, constraints))
 
 
 lines = open('input/day13.txt').readlines()
 layers = parse_input(lines)
 
 print('Part 1:', part1(layers))
+
+start = time.time()
 print('Part 2:', part2(layers))
+print('Part 2 execution time:', time.time() - start, 'seconds')
