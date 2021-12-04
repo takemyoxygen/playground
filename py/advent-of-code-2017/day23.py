67dd86d8be9fb93bb59ce94dd61c80bedbfa505f
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2021-12-04 16:44:08 +0300

    AOC 2017 - day23 part 2

--- py/advent-of-code-2017/day23.py
+++ py/advent-of-code-2017/day23.py
@@ -1,6 +1,6 @@
+from os import WUNTRACED
 from typing import List
 from collections import defaultdict
-import pprint
 
 from common.registers import handlers, Registers
 
@@ -19,24 +19,26 @@ def part1(instructions: List[List[str]]) -> int:
     return mul_count
 
 
-def format_registers(registers: Registers) -> str:
-    return ', '.join(map(lambda key: f'{key}: {registers[key]}', registers))
+def is_composite(x: int) -> bool:
+    for i in range(2, x):
+        if x % i == 0:
+            return True
+    return False
 
+def part2_code_optimized():
+    b = 109900
+    c = 126900
+    h = 0
 
-def part2(instructions: List[List[str]], max_steps=None) -> int:
-    registers = defaultdict(lambda: 0)
-    registers['a'] = 1
-    position = 0
-    steps = 0
-    while 0 <= position < len(instructions) and (max_steps is None or steps < max_steps):
-        [name, *args] = instructions[position]
-        position = handlers[name](registers, position, *args)
-        steps += 1
-        print(f'Step #{steps} after "{name} {" ".join(args)}", position: {position}, registers: {format_registers(registers)}')
-    return registers['h']
+    while True:
+        if is_composite(b):
+            h += 1
+        if b == c:
+            return h
+        b += 17
 
 
 instructions = [line.rstrip('\n').split(' ') for line in open('input/day23.txt').readlines()]
 
-# print('Part 1:', part1(instructions))
-print('Part 2:', part2(instructions, 100))
+print('Part 1:', part1(instructions))
+print('Part 2', part2_code_optimized())
